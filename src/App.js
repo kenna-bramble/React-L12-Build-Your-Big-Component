aef74830bd0cbe898a8c823f676337f2f89eb4f3
McKenna Bramble <77587638+kenna-bramble@users.noreply.github.com>
2022-10-19 13:38:59 -0400

    Added classNames to App.js

--- src/App.js
+++ src/App.js
@@ -3,27 +3,31 @@ import React from "react";
 const App = () => (
   <div>
     <header>
-      <h1>Super Sticky Notes</h1>
-      <aside>
-        <button>+ New Note</button>
-        <input type="text" placeholder="Type here to search..." />
+      <h1 className="app-header__title">Super Sticky Notes</h1>
+      <aside className="app-header__controls">
+        <button className="add-new">+ New Note</button>
+        <input
+          className="search"
+          type="text"
+          placeholder="Type here to search..."
+        />
       </aside>
     </header>
-    <ul>
-      <li>
-        <input type="text" placeholder="Title" />
-        <textarea placeholder="Description..." />
-        <span>X</span>
+    <ul className="notes-list">
+      <li className="note">
+        <input className="note__title" type="text" placeholder="Title" />
+        <textarea className="note__description" placeholder="Description..." />
+        <span className="note__delete">X</span>
       </li>
-      <li>
-        <input type="text" placeholder="Title" />
-        <textarea placeholder="Description..." />
-        <span>X</span>
+      <li className="note">
+        <input className="note__title" type="text" placeholder="Title" />
+        <textarea className="note__description" placeholder="Description..." />
+        <span className="note__delete">X</span>
       </li>
-      <li>
-        <input type="text" placeholder="Title" />
-        <textarea placeholder="Description..." />
-        <span>X</span>
+      <li className="note">
+        <input className="note__title" type="text" placeholder="Title" />
+        <textarea className="note__description" placeholder="Description..." />
+        <span className="note__delete">X</span>
       </li>
     </ul>
   </div>
