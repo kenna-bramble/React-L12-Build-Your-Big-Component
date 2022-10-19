716bfc7d662f1a21f28e2b035e78185686fdc1a9
McKenna Bramble <77587638+kenna-bramble@users.noreply.github.com>
2022-10-19 14:24:08 -0400

    Decomposed App Component; added Header.js, Note.js, NotesList.js

---
+++ src/NotesList.js
@@ -0,0 +1,12 @@
+import Note from "./Note.js";
+import React from "react";
+
+const NotesList = () => (
+  <ul className="notes-list">
+    <Note />
+    <Note />
+    <Note />
+  </ul>
+);
+
+export default NotesList;
